5cd625bdcfdfc409b8133d8da8ef480e5c7902fa
David Bedford <beds2004@hotmail.com>
2015-03-08 23:34:37 +0000

    Added in Angular

--- server.js
+++ server.js
@@ -43,14 +43,85 @@ app.get('/', function(req, res) {
 // get an instance of the express router
 var apiRouter = express.Router();
 
-// middleware to use for all requests
+// route to authenticate a user (POST http://localhost:8080/api/authenticate)
+apiRouter.post('/authenticate', function(req, res) {
+    // find the user
+    // select the name username and password explicitly
+    User.findOne({
+        username: req.body.username
+    }).select('name username password').exec(function(err, user) {
+        if (err) {
+            throw err;
+        }
+        
+        // no user with that username was found
+        if (!user) {
+            res.json({
+                success: false,
+                message: 'Authentication failed. User not found.'
+            });
+        } else if (user) {
+            // check if password matches
+            var validPassword = user.comparePassword(req.body.password);
+            
+            if (!validPassword) {
+                res.json({
+                    success: false,
+                    message: 'Authentication failed. Wrong password.'
+                });
+            } else {
+                // if user is found and password is right
+                // create a token
+                var token = jwt.sign({
+                    name: user.name,
+                    username: user.username
+                }, superSecret, {
+                    expiresInMinutes: 1440 // expires in 24 hours
+                });
+                
+                // return the information including token as JSON
+                res.json({
+                    success: true,
+                    message: 'Enjoy your token!',
+                    token: token
+                });
+            }
+        }
+    });
+});
+
+// route middleware to verify a token
 apiRouter.use(function(req, res, next) {
     // do logging
     console.log('Somebody just came to our app!');
     
-    // this is where we will authenticate users
+    // check header or url parameters or post parameters for token
+    var token = req.body.token || req.param('token') || req.headers['x-access-token'];
     
-    next(); // make sure we go to the next routes and don't stop here
+    // decode token
+    if (token) {
+        // verifies secret and checks exp
+        jwt.verify(token, superSecret, function(err, decoded) {
+            if (err) {
+                return res.status(403).send({
+                    success: false,
+                    message: 'Failed to authenticate token.'
+                });
+            } else {
+                // if everything is good, save to request for use in other routes
+                req.decoded = decoded;
+                
+                next();
+            }
+        });
+    } else {
+        // if there is no token
+        // return an HTTP response of 403 (access forbidden) and an error message
+        return res.status(403).send({
+            success: false,
+            message: 'No token provided.'
+        });
+    }
 });
 
 // test route to make sure everything is working
@@ -159,6 +230,11 @@ apiRouter.route('/users/:user_id')
             res.json({message: 'Successfully deleted'});
         });
     });
+
+// api endpoint to get user information
+apiRouter.get('/me', function(req, res) {
+    res.send(req.decoded);
+});
 
 // REGISTER OUR ROUTES
 // all of our routes will be prefixed with /api
