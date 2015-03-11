8d85946eae96f42e3b29fed0d0ad9b8c0719f4b0
David Bedford <beds2004@hotmail.com>
2015-03-11 00:02:25 +0000

    Modified directory structure

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'port': process.env.PORT || 8080,
+    'database': 'mongodb://djbedford:coventry@proximus.modulusmongo.net:27017/tOdo9hyb',
+    'secret': 'ilovescotchscotchyscotchscotch'
+};
