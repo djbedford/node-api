2a0f58f9d53e8f330124d7241028d5402cff1c14
David Bedford <beds2004@hotmail.com>
2015-03-14 11:21:11 +0000

    Add token to each http request

--- public/app/app.js
+++ public/app/app.js
@@ -5,4 +5,10 @@ angular.module('userApp', [
     'mainController',
     'userController',
     'userService'
-]);
+])
+
+// application configuration to integrate token into requests
+.config(function($httpProvider) {
+    // attach our auth interceptor to the http requests
+    $httpProvider.interceptors.push('AuthInterceptor');
+});
