14a925a99ada1694d1cc8b6b3cfb763586bc9c05
David Bedford <beds2004@hotmail.com>
2015-03-14 11:13:03 +0000

    Check successful login

--- public/app/controllers/mainController.js
+++ public/app/controllers/mainController.js
@@ -19,11 +19,21 @@ angular.module('mainController', [])
     
     // function to handle login form
     vm.doLogin = function() {
+        vm.processing = true;
+        
+        // clear the error
+        vm.error = '';
+        
         // call the Auth.login() function
         Auth.login(vm.loginData.username, vm.loginData.password)
             .success(function(data) {
+                vm.processing = false;
+            
                 // if a user successfully logs in, redirect to users page
+            if (data.success) {
                 $location.path('/users');
+            } else {
+                vm.error = data.message;
             });
     };
     
