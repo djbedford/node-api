8b31e1f58cdf6a6f511eb9bb9cea60142c6e1235
David Bedford <beds2004@hotmail.com>
2015-03-15 20:27:47 +0000

    Update to include user edit

--- public/app/app.routes.js
+++ public/app/app.routes.js
@@ -27,6 +27,13 @@ angular.module('app.routes', ['ngRoute'])
             templateUrl: 'app/views/pages/users/single.html',
             controller: 'userCreateController',
             controllerAs: 'user'
+        })
+    
+        // page to edit a user
+        .when('/users/:user_id', {
+            templateUrl: 'app/views/pages/users/single.html',
+            controller: 'userEditController',
+            controllerAs: 'user'
         });
     
     // get rid of the hash in the URL
--- public/app/controllers/userController.js
+++ public/app/controllers/userController.js
@@ -64,3 +64,37 @@ angular.module('userController', ['userService'])
             });
     }
 })
+
+// controller applied to user edit page
+.controller('userEditController', function($routeParams, User) {
+    var vm = this;
+    
+    // variable to hide/show elements of the view
+    // differentiates between create or edit pages
+    vm.type = 'edit';
+    
+    // get the user data for the user you want to edit
+    // $routeParams is the way we grab data from the URL
+    User.get($routeParams.user_id)
+        .success(function(data) {
+            vm.userData = data;
+        });
+    
+    // function to save the user
+    vm.saveUser = function() {
+        vm.processing = true;
+        vm.message = '';
+        
+        // call the userService function update
+        User.update($routeParams.user_id, vm.userData)
+            .success(function(data) {
+                vm.processing = false;
+            
+                // clear the form
+                vm.userData = {};
+            
+                // bind the message from our API to vm.message
+                vm.message = data.message;
+            });
+    };
+})
